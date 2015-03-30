1f2a1ef8e226e8949ef9c9f742251e72aaa89b41
Alan Berman <alan.berman.92@gmail.com>
2015-03-30 09:42:09 +0200

    Initial commit of HuffmanTree.h and HuffmanNode.h

---
+++ HuffmanNode.h
@@ -0,0 +1,24 @@
+//HuffmanNode.h
+//Author: Alan Berman
+//1/04/2015
+
+//Adapted from http://www.cprogramming.com/tutorial/lesson18.html
+#ifndef HUFFMANNODE_H
+#define HUFFMANNODE_H
+namespace BRMALA003 {
+	
+class HuffmanNode 
+{
+	private:
+	int value;
+	HuffmanNode *left;
+	HuffmanNode *right;
+	
+	
+}	
+	
+	
+	
+}
+#endif //HUFFMANNODE_H
+
---
+++ HuffmanTree.h
@@ -0,0 +1,24 @@
+//HuffmanTree.h
+//Author: Alan Berman
+//1/04/2015
+
+#ifndef HUFFMANTREE_H
+#define HUFFMANTREE_H
+namespace BRMALA003 {
+
+//Adapted from http://www.cprogramming.com/tutorial/lesson18.html
+class HuffmanTree
+{
+	private:
+	
+	public:
+	HuffmanTree();
+	~HuffmanTree();
+	
+}	
+	
+	
+	
+	
+}
+#endif //HUFFMANTREE_H
