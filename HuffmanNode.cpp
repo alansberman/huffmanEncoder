0dd2634733a6f3e6234b199acf4cf5532e45e17d
Alan Berman <alan.berman.92@gmail.com>
2015-03-30 14:54:06 +0200

    Initial commit of HuffmanTree.cpp and HuffmanNode.cpp

---
+++ HuffmanNode.cpp
@@ -0,0 +1,15 @@
+//HuffmanNode.cpp
+//Author: Alan Berman
+//1/4/2015
+#include "HuffmanTree.h"
+#include "HuffmanNode."
+#include <queue>
+#include <vector>
+#include <memory>
+namespace BRMALA003
+{
+
+
+
+
+}
---
+++ HuffmanTree.cpp
@@ -0,0 +1,15 @@
+//HuffmanTree.cpp
+//Author: Alan Berman
+//1/4/2015
+#include "HuffmanTree.h"
+#include "HuffmanNode.h"
+#include <queue>
+#include <vector>
+#include <memory>
+namespace BRMALA003
+{
+	Comparator::Comparator;
+
+
+
+}
