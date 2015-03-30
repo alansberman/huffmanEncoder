1e8cb2959c8a6d8f1a756307bf2b6ffa81b54b44
Alan Berman <alan.berman.92@gmail.com>
2015-03-30 13:10:15 +0200

    huffencode.cpp now creates an unordered_map containing the frequency of each char

--- huffencode.cpp
+++ huffencode.cpp
@@ -3,11 +3,11 @@
 //01/04/2015
 #include <string>
 #include <iostream>
+#include <unordered_map>
 #include <fstream>
-#include <sstream>
 #include "HuffmanNode.h"
 #include "HuffmanTree.h"
-#include <unordered_map>
+
 //Driver that makes
 using namespace std;
 int main(int argc, char * argv[])
@@ -16,21 +16,44 @@ int main(int argc, char * argv[])
 	//to an unordered_map
 	string inputFile = string(argv[1]);
 	string line;
+	char c;
 	ifstream i(inputFile.c_str());
 	unordered_map<char,int> charfreqmap;
+
 	if (!i)
 	{
-	  cout << "Couldn't open the file to decompres" << endl;
+	  cout << "Couldn't open the file to decompress" << endl;
 	  return -1;
 	}
 	//Read in each line of the txt file
 	while (!i.eof())
 	{
 		getline(i,line);
-	//	istringstream ss(info);
-	//	ss >> width;
-	//	ss >> height;
-	//	ss >> this->number_images;
+		for (unsigned int i=0;i<line.length();i++)
+		{
+			c=line.at(i);
+			//Insert a new char into the map 
+			if (charfreqmap.count(c)==0)
+			{
+				charfreqmap.insert({c,1});
+			}
+			else
+			{
+				//Add 1 to the frequency of the given char
+				//Adapted from http://www.cplusplus.com/reference/unordered_map/unordered_map/find/
+					unordered_map<char,int>::iterator found = charfreqmap.find(c);
+					if (found==charfreqmap.end())
+					{
+						cout << "Didn't find char " << c << " in the map" << endl;
+					}
+					else
+					{
+						found->second=found->second++;
+					}
+				
+			}	
+			
+		}
 	}
 	
 	return 0;
