baffb4ce0ea7b0baa56485c86b4f0da0fda6d6dd
Alan Berman <alan.berman.92@gmail.com>
2015-03-31 20:39:22 +0200

    Driver (huffencode) adds to the unordered_map correctly. Adding to the priority_queue in progress

--- huffencode.cpp
+++ huffencode.cpp
@@ -5,19 +5,21 @@
 #include <iostream>
 #include <queue>
 #include <unordered_map>
+#include <memory>
 #include <fstream>
-#include "HuffmanNode.h"
-#include "HuffmanTree.h"
+#include "HuffmanTree.cpp"
+#include "HuffmanNode.cpp"
 
 //Driver that makes
 using namespace std;
-int main(int argc, char * argv[])
+int main(int argc, char * argv[]) //
 {
 	//Read in the txt file to compress and add each char
 	//to an unordered_map
-	string inputFile = string(argv[1]);
+	//string inputFile = string(argv[1]); /////////////////uncomment this
+	string inputFile = "test.txt";
 	BRMALA003::HuffmanTree tree;
-	//BRMALA003::HuffmanNode node;
+	//BRMALA003::HuffmanNode() node;
 	string line;
 	char c;
 	ifstream i(inputFile.c_str());
@@ -36,8 +38,7 @@ int main(int argc, char * argv[])
 			c=line.at(i);
 			//Insert a new char into the map 
 			if (charfreqmap.count(c)==0)
-			{
-				BRMALA003::HuffmanNode node
+			{				
 				charfreqmap.insert({c,1});
 			}
 			else
@@ -47,7 +48,7 @@ int main(int argc, char * argv[])
 					unordered_map<char,int>::iterator found = charfreqmap.find(c);
 					if (found==charfreqmap.end())
 					{
-						cout << "Didn't find char " << c << " in the map" << endl;
+						cerr << "Didn't find char " << c << " in the map" << endl;
 					}
 					else
 					{
@@ -58,6 +59,21 @@ int main(int argc, char * argv[])
 			}	
 			
 		}
+	}
+	//For each letter, make a HuffmanNOde and add it to the 
+	for (auto it=charfreqmap.begin();it!=charfreqmap.end();++it)
+	{
+		cout << it->first << " is the letter, " << it->second << " is its frequency" << endl;
+		BRMALA003::HuffmanNode node = BRMALA003::HuffmanNode(it->first,it->second);
+		shared_ptr<BRMALA003::HuffmanNode> nodeptr = make_shared<BRMALA003::HuffmanNode>(node) ; //(&node)
+		tree.getQueue().push(nodeptr);
+		cout << tree.getQueue().size() << " is the size " << endl;
+		
+	}
+	
+	while (tree.getQueue().size()!=0)
+	{
+		cout << tree.getQueue().top()->getFrequency() <<  " is the frequency " << endl;
 	}
 	
 	return 0;
