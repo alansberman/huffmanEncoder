12da8afbc7c1c13552fda09a8eb0eb609215ec95
Alan Berman <alan.berman.92@gmail.com>
2015-04-05 13:09:27 +0200

    Moved the code table functionality to a separate file with its own functionality

---
+++ CodeTable.cpp
@@ -0,0 +1,55 @@
+//CodeTable.cpp
+//Author: Alan Berman
+//5/4/2015
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include "CodeTable.h"
+#include "HuffmanNode.h"
+//Class that makes a code table using a HuffmanTree
+namespace BRMALA003
+{
+	using namespace std;
+	CodeTable::CodeTable()
+	{
+		buffer="";
+	}
+	unordered_map<char,string>& CodeTable::getCodeTable()
+	{
+		return codeTable;
+	}
+	//Recursive method that creates the code table,
+	//using inorder traversal of the HuffmanTree
+	
+	void CodeTable::createCodeTable(shared_ptr<HuffmanNode> nodeptr, string code)
+	{
+		//If the pointer to the node is not null
+		if (nodeptr)
+		{
+			//Do the node's left child
+			CodeTable::createCodeTable(nodeptr->getLeft(),code+"0");
+			//If the node is a leaf (i.e. has a character),
+			//insert it into the code table
+			if (nodeptr->getLeft()==NULL && nodeptr->getRight() == NULL)
+			{
+				CodeTable::getCodeTable().insert({nodeptr->getLetter(),code});
+				
+			}
+			//Do the node's right child
+			CodeTable::createCodeTable(nodeptr->getRight(),code+"1");
+		}
+	}
+	string& CodeTable::getBuffer()
+	{
+		return buffer;
+	}
+	void CodeTable::setBuffer()
+	{
+		//Set the buffer string
+		for (auto i=getCodeTable().begin();i!=getCodeTable().end();++i)
+		{
+			buffer+=i->second;
+		}
+	}
+
+}
---
+++ CodeTable.h
@@ -0,0 +1,34 @@
+//CodeTable.h
+//Author: Alan Berman
+//5/04/2015
+
+//CodeTable header file
+#ifndef CODETABLE_H
+#define CODETABLE_H
+#include <string>
+#include <memory>
+#include <unordered_map>
+#include "HuffmanNode.h"
+namespace BRMALA003 {
+using namespace std;	
+//Class that makes a code table using a HuffmanTree
+class CodeTable 
+{
+	private:
+	unordered_map<char,string> codeTable;
+	string buffer;
+	public:
+	CodeTable();
+	CodeTable(const CodeTable & rhs) = default; //Copy Constructor
+	CodeTable(CodeTable && rhs) = default; //Move Constructor
+	//Copy and Move Assignment Operators
+	CodeTable & operator=(const CodeTable & rhs) = default;
+	CodeTable & operator=(CodeTable && rhs) = default;
+	~CodeTable(void) = default; //Deconstructor
+	unordered_map<char,string>& getCodeTable();
+	string& getBuffer();
+	void setBuffer();
+	void createCodeTable(shared_ptr<HuffmanNode> nodeptr, string code);
+};		
+}
+#endif //CODETABLE_H
